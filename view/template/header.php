23c797f8766fd5b8df2b589e76058ab9c9c1b769
Diego Tutin <diegotutin3255@gmail.com>
2022-08-22 10:08:07 -0500

    Cambio de un nombre

--- view/template/header.php
+++ view/template/header.php
@@ -243,7 +243,7 @@
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="../actores/index.php" class="nav-link ">
+                                    <a href="../docentes/index.php" class="nav-link ">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Docentes</p>
                                     </a>

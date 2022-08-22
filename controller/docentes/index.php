616b48e68ed0133d1283af97ab114054501ceebb
Diego Tutin <diegotutin3255@gmail.com>
2022-08-22 18:33:25 -0500

    Tabla Generos Terminada

--- controller/docentes/index.php
+++ controller/docentes/index.php
@@ -2,9 +2,10 @@
     include '../../model/conectar.php';
     
     $sql = "SELECT do.id_doc , do.nombre_doc, do.nombre_doc, do.apellido_doc, do.titulo_doc,
-    ca.nombre_carr
-    FROM docentes do, carrera ca
-    WHERE do.codigo_carr = ca.codigo_carr";
+    ca.nombre_carr, capa.nombre_capa
+    FROM docentes do, carrera ca, capacitacion capa
+    WHERE do.codigo_carr = ca.codigo_carr
+    AND do.codigo_capa = capa.codigo_capa";
     $result = $conn->query($sql);
     include '../../model/desconectar.php';
 ?>
